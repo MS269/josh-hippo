5afad287f53c7b6dc3d0c27e857dffaba90e0ee2
MS269 <ehddnr9797@gmail.com>
2023-11-23 21:26:42 -0500

    feat: verify email page

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,9 +2,20 @@ import dotenv from "dotenv";
 import path from "path";
 import type { InitOptions } from "payload/config";
 import payload, { type Payload } from "payload";
+import nodemailer from "nodemailer";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
+});
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY,
+  },
 });
 
 let cached = (global as any).payload;
@@ -24,7 +35,7 @@ export const getPayloadClient = async ({
   initOptions,
 }: Args = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
-    throw new Error("PAYLOAD_SECRET is missing.");
+    throw new Error("PAYLOAD_SECRET is missing");
   }
 
   if (cached.client) {
@@ -33,6 +44,11 @@ export const getPayloadClient = async ({
 
   if (!cached.promise) {
     cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "hello@joshtriedcoding.com",
+        fromName: "DigitalHippo",
+      },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
