9721f02fed47a13361626152501c4a476751482f
MS269 <ehddnr9797@gmail.com>
2023-12-22 17:09:22 -0500

    feat: stripe

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,7 +1,23 @@
+import type { User } from "@/payload-types";
 import type { ExpressContext } from "@/server";
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
+import type { PayloadRequest } from "payload/types";
 
 const t = initTRPC.context<ExpressContext>().create();
+const middleware = t.middleware;
+
+const isAuth = middleware(async ({ ctx, next }) => {
+  const req = ctx.req as PayloadRequest;
+
+  const { user } = req as { user: User | null };
+
+  if (!user || !user.id) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({ ctx: { user } });
+});
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const privateProcdure = t.procedure.use(isAuth);
